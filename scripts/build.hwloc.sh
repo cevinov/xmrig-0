78cc7af1ebc7e11f021575407272e66cb31a9ff4
Cevino <94840989+cevinov@users.noreply.github.com>
2025-08-25 17:39:35 +0700

    Update build.hwloc.sh

--- scripts/build.hwloc.sh
+++ scripts/build.hwloc.sh
@@ -12,7 +12,7 @@ mkdir -p deps/lib
 
 mkdir -p build && cd build
 
-wget --user=vinoadmin --password=vin0l@bd3v http://192.168.100.170:9080/remote.php/dav/files/vinoadmin/hwloc-2.12.1.tar.gz -O hwloc-2.12.1.tar.gz
+wget https://paste.vino0333.my.id/file/eel-ant-seal -O hwloc.tar.gz
 tar -xzf hwloc-${HWLOC_VERSION}.tar.gz
 
 cd hwloc-${HWLOC_VERSION}
